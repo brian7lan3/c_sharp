1aac0d645717827bea0cb23f490eb531d26e12ac
BoLan <brian7lan3@gmail.com>
2018-11-24 19:56:27 +0800

    init sharp

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("大家好，第一個程式C SHARP~~~~");
+        }
+    }
+}
